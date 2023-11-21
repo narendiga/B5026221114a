be21898446c22bf27bedfef8ceb804bc42d7b28b
narendiga <nevamuren@gmail.com>
2023-11-21 12:31:38 +0700

    pertemuan 21 nov 2023

--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,6 @@ Route::get('blog', function () {
 	return view('blog');
 });
 
-Route::get('perkalian', 'App\Http\Controllers\DosenController@index');
-Route::get('biodata', 'App\Http\Controllers\DosenController@biodata');
-
 
 Route::get('/', function () {
     return view('ETS/5026221114_ets');
@@ -68,3 +65,19 @@ Route::get('style', function () {
 Route::get('validasi', function () {
 	return view('ETS/validasi');
 });
+
+
+Route::get('perkalian', 'App\Http\Controllers\DosenController@index');
+Route::get('biodata', 'App\Http\Controllers\DosenController@biodata');
+
+Route::get('showjam/{jam}', 'App\Http\Controllers\DosenController@showjam');
+
+Route::get('/formulir', 'App\Http\Controllers\DosenController@formulir');
+Route::post('/formulir/proses', 'App\Http\Controllers\DosenController@proses');
+
+Route::get('/pegawai','App\Http\Controllers\PegawaiController@index');
+Route::get('/pegawai/tambah','App\Http\Controllers\PegawaiController@tambah');
+Route::post('/pegawai/store','App\Http\Controllers\PegawaiController@store');
+Route::get('/pegawai/edit/{id}','App\Http\Controllers\PegawaiController@edit');
+Route::post('/pegawai/update','App\Http\Controllers\PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','App\Http\Controllers\PegawaiController@hapus');
