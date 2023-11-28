58cfca1b6e87937ebb71d9125fa723e73a017e0a
narendiga <nevamuren@gmail.com>
2023-11-28 12:38:14 +0700

    Pertemuan Tanggal 28 November 2023 2

--- resources/views/tambah.blade.php
+++ resources/views/tambah.blade.php
@@ -1,29 +1,29 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
-</head>
-<body>
+@extends('master')
 
-	<h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
-	<h3>Edit Pegawai</h3>
+@section('title', 'Data Pegawai')
 
-	<a href="/pegawai"> Kembali</a>
+@section('konten')
+    <h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
+    <h3>Edit Pegawai</h3>
 
-	<br/>
-	<br/>
+    <a href="/pegawai"> Kembali</a>
 
-	@foreach($pegawai as $p)
-	<form action="/pegawai/update" method="post">
+    <br />
+    <br />
+
+    <form action="/pegawai/store" method="post" class="form-horizontal">
 		{{ csrf_field() }}
-		<input type="hidden" name="id" value="{{ $p->pegawai_id }}"> <br/>
-		Nama <input type="text" required="required" name="nama" value="{{ $p->pegawai_nama }}"> <br/>
-		Jabatan <input type="text" required="required" name="jabatan" value="{{ $p->pegawai_jabatan }}"> <br/>
-		Umur <input type="number" required="required" name="umur" value="{{ $p->pegawai_umur }}"> <br/>
-		Alamat <textarea required="required" name="alamat">{{ $p->pegawai_alamat }}</textarea> <br/>
+		<div class="form-group row">
+            <label for="nama" class="col-xs-3 col-form-label mr-2">Nama</label>
+            <div class="col-xs-9">
+            <input type="text" class="form-control" id="nama" name="nama">
+            </div>
+        </div>
+		Nama <input type="text" name="nama"> <br/>
+		Jabatan <input type="text" name="jabatan"> <br/>
+		Umur <input type="number" name="umur"> <br/>
+		Alamat <textarea name="alamat"></textarea> <br/>
 		<input type="submit" value="Simpan Data">
 	</form>
-	@endforeach
 
-</body>
-</html>
+@endsection
