eaaa4a77b0a856ada913d732c3db4a54ef68a20b
narendiga <nevamuren@gmail.com>
2023-12-04 10:19:21 +0700

    Tugas Pertemuan Tanggal 27 Nop

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -12,15 +12,51 @@
     <br />
 
     @foreach ($pegawai as $p)
-        <form action="/pegawai/update" method="post">
+        <form action="/pegawai/update" method="post" class="form-horizontal" role="form">
             {{ csrf_field() }}
-            <input type="hidden" name="id" value="{{ $p->pegawai_id }}"> <br />
-            Nama <input type="text" required="required" name="nama" value="{{ $p->pegawai_nama }}"> <br />
-            Jabatan <input type="text" required="required" name="jabatan" value="{{ $p->pegawai_jabatan }}"> <br />
-            Umur <input type="number" required="required" name="umur" value="{{ $p->pegawai_umur }}"> <br />
-            Alamat
-            <textarea required="required" name="alamat">{{ $p->pegawai_alamat }}</textarea> <br />
-            <input type="submit" value="Simpan Data">
+
+            <input type="hidden" name="id" value="{{ $p->pegawai_id }}">
+
+            <div class = "form-group row">
+                <label for = "nama" class = "col-sm-2 control-label">Nama</label>
+
+                <div class = "col-sm-10">
+                    <input type="text" required="required" name="nama" value="{{ $p->pegawai_nama }}"
+                        class="form-control">
+                </div>
+
+            </div>
+
+            <div class = "form-group row">
+                <label for = "jabatan" class = "col-sm-2 control-label">Jabatan</label>
+
+                <div class = "col-sm-10">
+                    <input type="text" required="required" name="jabatan" value="{{ $p->pegawai_jabatan }}"
+                        class="form-control">
+                </div>
+
+            </div>
+
+            <div class = "form-group row">
+                <label for = "umur" class = "col-sm-2 control-label">Umur</label>
+
+                <div class = "col-sm-10">
+                    <input type="number" required="required" name="umur" value="{{ $p->pegawai_umur }}"
+                        class="form-control">
+                </div>
+
+            </div>
+
+            <div class = "form-group row">
+                <label for = "alamat" class = "col-sm-2 control-label">Alamat</label>
+
+                <div class = "col-sm-10">
+                    <textarea required="required" name="alamat" class="form-control">{{ $p->pegawai_alamat }}</textarea>
+                </div>
+
+            </div>
+
+            <input type="submit" value="Simpan Data" class="btn btn-primary">
         </form>
     @endforeach
 @endsection
--- resources/views/tambah.blade.php
+++ resources/views/tambah.blade.php
@@ -12,18 +12,46 @@
     <br />
 
     <form action="/pegawai/store" method="post" class="form-horizontal">
-		{{ csrf_field() }}
-		<div class="form-group row">
-            <label for="nama" class="col-xs-3 col-form-label mr-2">Nama</label>
-            <div class="col-xs-9">
-            <input type="text" class="form-control" id="nama" name="nama">
+        {{ csrf_field() }}
+
+        <div class = "form-group row">
+            <label for = "firstname" class = "col-sm-2 control-label">Nama</label>
+            <div class = "col-sm-10">
+                <input name="nama" type = "text" class = "form-control" id = "nama"
+                    placeholder = "Masukkan Nama Pegawai">
             </div>
         </div>
-		Nama <input type="text" name="nama"> <br/>
-		Jabatan <input type="text" name="jabatan"> <br/>
-		Umur <input type="number" name="umur"> <br/>
-		Alamat <textarea name="alamat"></textarea> <br/>
-		<input type="submit" value="Simpan Data">
-	</form>
+
+        <div class = "form-group row">
+            <label for = "jabatan" class = "col-sm-2 control-label">Jabatan</label>
+
+            <div class = "col-sm-10">
+                <input name="jabatan" type = "text" class = "form-control" id = "jabatan"
+                    placeholder = "Masukkan Jabatan Pegawai">
+            </div>
+
+        </div>
+
+        <div class = "form-group row">
+            <label for = "umur" class = "col-sm-2 control-label">Umur</label>
+
+            <div class = "col-sm-10">
+                <input name="umur" type = "number" class = "form-control" id = "umur"
+                    placeholder = "Masukkan Umur Pegawai">
+            </div>
+
+        </div>
+
+        <div class = "form-group row">
+            <label for = "alamat" class = "col-sm-2 control-label">Alamat</label>
+
+            <div class = "col-sm-10">
+                <textarea name="alamat" class = "form-control" id = "jabatan" placeholder = "Masukkan Alamat Pegawai"></textarea>
+            </div>
+
+        </div>
+
+        <input type="submit" value="Simpan Data" class="btn btn-primary">
+    </form>
 
 @endsection
